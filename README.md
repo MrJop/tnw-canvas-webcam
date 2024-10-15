36e19eef3d383aadc220507ed685af50f6140501
MrJop <jop@interactionfigure.nl>
2024-10-15 09:12:17 +0200

    first files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# tnw-canvas-webcam
+# Code Studio Webcam/Canvas demo
 Example of using Canvas and the Webcam
---
+++ js/code.js
@@ -0,0 +1,11 @@
+var $ = jQuery.noConflict();
+
+$(document).ready(function(e) {
+    CanvasExample.init();
+});
+
+var CanvasExample = {
+    init: function () {
+
+    }
+}
